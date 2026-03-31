7e7f96a0d6639d960e73e5f354de8a845be112d6
Raimund <21100416+RaimundHeid@users.noreply.github.com>
2026-03-31 20:41:03 +0200

    version number increased

--- src/protector.c
+++ src/protector.c
@@ -42,7 +42,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *programVersionNumber = "2.1";
+const char *programVersionNumber = "2.2";
 
 int _distance[_64_][_64_];
 int _horizontalDistance[_64_][_64_];
